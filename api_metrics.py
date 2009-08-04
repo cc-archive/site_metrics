d522cfeb7b00d01f09975f6fed55ec35e05b1aef
unknown <tobiaf2@.(none)>
2009-08-04 18:25:27 -0400

    Refactor slightly.

--- api_metrics.py
+++ api_metrics.py
@@ -3,14 +3,24 @@ import time
 import api_parse
 import api_analyze
 
-parse_start = time.time()
-data = api_parse.run()
-parse_stop = time.time()
+def run_all():
+    parse_start = time.time()
+    data = api_parse.parse()
+    parse_stop = time.time()
 
-analysis_start = time.time()
-api_analyze.analyze(data)
-analysis_stop = time.time()
+    analysis_start = time.time()
+    api_analyze.analyze(data)
+    analysis_stop = time.time()
 
-print "============================="
-print "Parse time: %.2f seconds" % (parse_stop - parse_start)
-print "Analysis time: %.2f seconds" % (analysis_stop - analysis_start)
+    print "============================="
+    print "Parse time: %.2f seconds" % (parse_stop - parse_start)
+    print "Analysis time: %.2f seconds" % (analysis_stop - analysis_start)
+
+def run_test():
+    data = api_parse.minimal_parse()
+    f = open('paths.txt','w')
+    for d in data:
+        f.write("%s %s\n" % (d[0], d[1]))
+
+if __name__ == '__main__':
+    run_test()
