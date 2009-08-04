0ad51aaf11c9961cf14cee35535d2feff64539be
unknown <tobiaf2@.(none)>
2009-08-04 18:24:59 -0400

    Simplifying reporting.

--- api_analyze.py
+++ api_analyze.py
@@ -84,24 +84,8 @@ def foo(data):
     g['rest_paths_pct'] = float(g['rest_paths_num']*100) / g['records_num']
 
 def report_analysis():
-    print "%d individual records" % g['records_num']
-    print "%d records with '/rest' paths (%.2f%%)" % (g['rest_paths_num'],
-                                    g['rest_paths_pct'])
-    print "%d records with query strings (%.2f%%)" % (g['query_strings_num'], 
-                                    g['query_strings_pct'])
-    print
-    #print "Breakdown by version:"
-    #path_opt_items = path_option_counts.items()
-    #path_opt_items.sort(lambda x,y: cmp(x[1],y[1]), reverse=True)
-    #for key, value in path_opt_items:
-    #    print "  %s: %d (%.3f%%)" % (key, value, float(value*100) / rest_paths_num)
-    #print
-    #print "Option counts as follows (percents out of query string total):"
-    #query_opt_items = query_option_counts.items()
-    #query_opt_items.sort(lambda x,y: cmp(x[1],y[1]), reverse=True)
-    #for key, value in query_opt_items:
-    #    print "  %s: %d (%.3f%%)" % (key, value, 
-    #                                float(value*100)/query_strings_num)
+    for key, value in g['path']['items']:
+        print "%s -- %s" % (key, value)
 
 def _item_sort(x,y):
     return cmp(x[1],y[1])
