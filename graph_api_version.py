e5e794b6aeb24518887742051e3763ff7fdaea58
unknown <tobiaf2@.(none)>
2009-09-29 13:48:12 -0400

    Fix stats reporting.

--- graph_api_version.py
+++ graph_api_version.py
@@ -8,7 +8,8 @@ data = logparse.load()
 totals = dict()
 for key in data.keys():
     uris  = [urlparse.urlparse(s[1]) for s in data[key]]
-    versions = [u.path.split('/')[2] for u in uris if u.path.count('/') >= 2]
+    versions = [u.path.split('/')[2].rstrip()
+                for u in uris if u.path.count('/') >= 2]
 
     #for dstr, ustr in data[key]:
     #    if ustr.startswith('/rest/'):
@@ -16,10 +17,23 @@ for key in data.keys():
     #        try:
     #            versions.append
         # dev, 1.0, 1.5
-    totals[key] = {}
+    totals[key] = dict()
     for v in set(versions):
         totals[key][v] = versions.count(v)
 
-print totals
+# the only keys that should matter are:
+
+valid = ('dev','1.5','1.0')
+for key, value in totals.items():
+    print '%s:' % key
+    for v in valid:
+        print '  %s - %d' % (v, value[v])
+    invalid_count = 0
+    for key2, value2 in value.items():
+        if key2 not in valid:
+            invalid_count += value2
+    print '  invalid - %d' % invalid_count
+
+
 # do some totals
 # then graph them
