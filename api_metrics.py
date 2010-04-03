dd4b1a5fe62f4d01a4b56b01e5082198f4c3b570
Frank Tobia <frank.tobia@gmail.com>
2010-04-03 12:24:05 -0400

    TWeak old api_metrics script to make it run.

--- api_metrics.py
+++ api_metrics.py
@@ -1,11 +1,11 @@
 import time
 
-import api_parse
+import logparse
 import api_analyze
 
 def run_all():
     parse_start = time.time()
-    data = api_parse.parse()
+    data = logparse.parse()
     parse_stop = time.time()
 
     analysis_start = time.time()
@@ -16,11 +16,11 @@ def run_all():
     print "Parse time: %.2f seconds" % (parse_stop - parse_start)
     print "Analysis time: %.2f seconds" % (analysis_stop - analysis_start)
 
-def run_test():
-    data = api_parse.minimal_parse()
+def run_test(): # not really sure what was going on here...
+    data = logparse.parse()#api_parse.minimal_parse()
     f = open('paths.txt','w')
     for d in data:
         f.write("%s %s\n" % (d[0], d[1]))
 
 if __name__ == '__main__':
-    run_test()
+    run_all()
