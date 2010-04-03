bac2931ee35f0ec792f466ff7d8642e08372297f
Frank Tobia <frank.tobia@gmail.com>
2010-04-03 14:00:39 -0400

    Get an initial version plot script working.

--- version_plot.py
+++ version_plot.py
@@ -1,12 +1,27 @@
 from matplotlib.pyplot import *
 import slogparse
+from math import log
 
 if __name__ == '__main__':
-    stats = slogparse.parse_logfiles()
-    dates = [stat._name for stat in stats] # get the dates
-    graphs = ['nonexistent', '1.0', '1.5', 'dev', 'invalid' ]
+    inlogs = True
+    stats = slogparse.read_stats()
+    fields = ['date','nonexistent','invalid','1.0','1.5','dev']
+
+    ddicts = []
+    for row in stats:
+        ddict = dict()
+        for i, field in zip(range(len(fields)), fields):
+            ddict[field] = row[i]
+        ddicts.append(ddict)
+
+    # sort just in case
+    ddicts.sort(lambda x,y: cmp(x['date'], y['date']))
+
+    graphs = ['nonexistent', '1.0', '1.5', 'dev', 'invalid']
     for graph in graphs:
-        plot([stat._processed_data['versions'][graph] for stat in stats],
-              label=graph)
+        if inlogs:
+            plot([log(float(ddict[graph]),10) for ddict in ddicts], label=graph)
+        else:
+            plot([ddict[graph] for ddict in ddicts], label=graph) 
     legend()
     show()
